e06a8d43db1dd34f87f9050f08a7694dc6d429a8
Shriyansh Gupta <gshriyansh703@gmail.com>
2025-02-21 19:19:41 +0530

    [Shriyansh Gupta] Add. Hello from Path Variable

--- spring_intro/src/main/java/com/capgemini/spring_intro/controller/MyController.java
+++ spring_intro/src/main/java/com/capgemini/spring_intro/controller/MyController.java
@@ -3,6 +3,7 @@ package com.capgemini.spring_intro.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,10 @@ public class MyController {
     @GetMapping("/hello2")
     public ResponseEntity<String> hello2(@RequestParam("value") String name){
         return new ResponseEntity<>("Hello " + name + " from Bridgelabz", HttpStatus.OK);
+    }
+
+    @GetMapping("/hello3/{name}")
+    public ResponseEntity<String> hello3(@PathVariable("name") String name){
+        return new ResponseEntity<>("Hello  " + name + " from BridgeLabz", HttpStatus.OK );
     }
 }
